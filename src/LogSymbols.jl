d54c3c39c9fa88b0c326d7acfacb65ad66b63c64
Mubaris NK <mubarishassannk@gmail.com>
2017-10-18 12:18:45 +0530

    Test 1

--- src/LogSymbols.jl
+++ src/LogSymbols.jl
@@ -2,4 +2,13 @@ module LogSymbols
 
 # package code goes here
 
+function print_rbg()
+    println("\e[1m\e[38;2;$r;$g;$b;249m",t)
+end
+
+symbols = Dict("info" => print_rbg(0, 0, 255, "ℹ"),
+                "success" => print_rbg(0, 255, 0, "✔"),
+                "warning" => print_rbg(255, 255, 0, "⚠"),
+                "error" => print_rbg(255, 0, 0), "✖")
+
 end # module
