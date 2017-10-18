5c3e40cb00e76f4c0ca67579db010364bf37c76a
Mubaris NK <mubarishassannk@gmail.com>
2017-10-18 12:31:27 +0530

    Change dict to vars

--- src/LogSymbols.jl
+++ src/LogSymbols.jl
@@ -6,9 +6,9 @@ function print_rbg(r, g, b, t)
     println("\e[1m\e[38;2;$r;$g;$b;249m",t)
 end
 
-symbols = Dict("info" => print_rbg(0, 0, 255, "ℹ"),
-                "success" => print_rbg(0, 255, 0, "✔"),
-                "warning" => print_rbg(255, 255, 0, "⚠"),
-                "error" => print_rbg(255, 0, 0, "✖"))
+info = print_rbg(0, 0, 255, "ℹ")
+success = print_rbg(0, 255, 0, "✔")
+warning = print_rbg(255, 255, 0, "⚠")
+error = print_rbg(255, 0, 0, "✖")
 
 end # module
