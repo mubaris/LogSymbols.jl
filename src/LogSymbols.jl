8e54f4934c2549055207584691a8918bb4e1c783
Mubaris NK <mubarishassannk@gmail.com>
2017-10-18 17:22:42 +0530

    Change to ANSI Colors

--- src/LogSymbols.jl
+++ src/LogSymbols.jl
@@ -2,13 +2,9 @@ module LogSymbols
 
 # package code goes here
 
-function print_rbg(r, g, b, t)
-    println("\e[1m\e[38;2;$r;$g;$b;249m",t)
-end
-
-info = print_rbg(0, 0, 255, "ℹ")
-success = print_rbg(0, 255, 0, "✔")
-warning = print_rbg(255, 255, 0, "⚠")
-error = print_rbg(255, 0, 0, "✖")
+info = "\u001b[32m ℹ \u001b[0m"
+success = "\u001b[32m ✔ \u001b[0m"
+warning = "\u001b[33m ⚠ \u001b[0m"
+error = "\u001b[31m ✖ \u001b[0m"
 
 end # module
